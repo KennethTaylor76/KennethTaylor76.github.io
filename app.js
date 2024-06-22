a3c23c40e42d0ee75e3a44d82af3349e73813b00
KennethTaylor76 <165370815+KennethTaylor76@users.noreply.github.com>
2024-06-22 14:43:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-
 let tg = window.Telegram.WebApp;
 
 const elements = document.getElementsByClassName("scroller_text");
 if (tg.colorScheme == "light"){
-    document.getElementById("header_container").style.color = "black";
     for (let i = 0; i < elements.length; i++) {
     elements[i].style.color = "black"; 
     }
 }
 else if (tg.colorScheme == "dark"){
-    document.getElementById("header_container").style.color = "white";
     for (let i = 0; i < elements.length; i++) {
     elements[i].style.color = "white"; 
     }
